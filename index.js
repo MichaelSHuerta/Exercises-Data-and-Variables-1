9950db34202fe636cade4dcf86ebe55073f6c91c
MichaelHuerta <85526782+MichaelSHuerta@users.noreply.github.com>
2021-06-10 21:54:00 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+//Declare and assign the variables here:
+
+letnameOfTheSpaceShuttle = "Determination"
+letshuttleSpeedmph=17500
+letDistanceToMarskm=225000000
+letDistanceToTheMoonkm=384400
+constmilesperkilometer=0.621
+
+//use console.log to print the 'typeof'each variable.Print one item per line
+
+console.log(typeof Number ("17500"))
+console.log(typeof Number("225000000"))
+console.log(typeof Number("384400"))
+console.log(typeof Number("0.621"))
+
+//code your solution to exercises 3 and 4 here:
+letmilesToMars=225000000*0.621
+
+lethoursToMars=139725000/17500
+letdaysToMars=7984.285714285715/24
+
+console.log("Determination will take 332.6 days to reach Mars")
+
+//code your solution to exercise 5 here:
+
+letmilesToMoon=384440*0.621
+lethoursToMoon=238737.24/17500
+letdaysToMoon=13.642128/24
+console.log("Determination will take 1/2 day to reach the moon")
+
+
